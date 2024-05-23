3bcb6a480c7d5423ac761230b2c73ce4dd40392b
setrasp <pontgil096@gmail.com>
2024-05-23 11:57:24 -0300

    Aula sobre operadores logicos

---
+++ javascript/main.js
@@ -0,0 +1,75 @@
+// Aqui eu tenho um comentario
+console.log("Meu nome é Gil")
+
+var nome  = "Setra"
+console.log("Imprimir Nome:")
+console.log(nome)
+
+// texto -> String
+// numeros inteiros -> Int
+// numeros decimais -> Float / Double
+// logicos -> Boolean(false : True)
+
+var telefone = "124 2424887-4584"
+var valormonetario = 54.87
+var valorinteiro = 12
+var verdadeirofalso = true
+
+// Guarde os dados do seu endereço em VARIAVEIS (ex. rua, bairro, cep, estado...)
+// Guarde os seus dados (nome, email, telefone, data nascimento) em VARIAVEIS 
+// Crie uma VARIAVEL que diz se você é mair de idade
+// Crie uma VARIAVEL uqe diz se você pode dirigir
+
+
+
+var nome  = "Aparecida Tognetta Bassette"
+console.log("Nome da Rua:" ,nome)
+
+var nome  = "Jd das Orquídeas"
+console.log("Bairro:" ,nome)
+
+var nome  = "127"
+console.log("Numero:" ,nome)
+
+var nome  = "13470755"
+console.log("Cep:" ,nome)
+
+var nome  = "Americana"
+console.log("Cidade:" ,nome)
+
+var nome  = "São Paulo"
+console.log("Estado:" ,nome)
+
+
+
+var nome  = "Gil Pont Gonçalves Setra"
+console.log("Meu Nome:" ,nome)
+
+var nome  = "Pontgil0246@gmail.com"
+console.log("E-mail:" ,nome)
+
+var nome  = "+55 (0124) 24248851581"
+console.log("Celular:" ,nome)
+
+var nome  = "15/05/2003"
+console.log("Data de Nascimento:" ,nome)
+
+var texto = "aqui eu tenho uma string"
+var verdadeirofalso = false
+var inteiro = 224
+var quebrado = 20.2
+
+
+
+var numero4 = 24
+console.log("Tabuada do 24")
+console.log("24x1 =",numero4*1)
+console.log("24x2 =",numero4*2)
+console.log("24x3 =",numero4*3)
+console.log("24x4 =",numero4*4)
+console.log("24x5 =",numero4*5)
+console.log("24x6 =",numero4*6)
+console.log("24x7 =",numero4*7)
+console.log("24x8 =",numero4*8)
+console.log("24x9 =",numero4*9)
+console.log("24x10 =",numero4*10)
