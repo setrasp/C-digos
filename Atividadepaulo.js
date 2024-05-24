24fcded1c342f00b90c4941875273c6ab76906f1
setrasp <pontgil096@gmail.com>
2024-05-24 09:47:40 -0300

    Atividade 24/05/2024

---
+++ Atividadepaulo.js
@@ -0,0 +1,42 @@
+console.log ("ATIVIDADE 1")
+console.log ("")
+var numero1, numero2
+
+numero1 = 7
+numero2 = 5
+
+console.log (numero1 + numero2)
+console.log (numero1 - numero2)
+console.log (numero1 * numero2)
+console.log (numero1 / numero2)
+console.log ("")
+
+console.log ("ATIVIDADE 2 - 3")
+console.log ("")
+var celsius,fahrenheit
+
+fahrenheit = 135
+celsius = 30.2
+
+var valorFahrenheit = fahrenheit-32
+var valorCelsius = celsius*1.8
+
+console.log(valorFahrenheit/1.8)
+console.log(valorCelsius+32)
+console.log ("")
+
+console.log ("ATIVIDADE 4")
+console.log ("")
+
+var metros = 5; 
+var centimetros = metros * 100; 
+console.log(metros + " metros é igual a " + centimetros + " centímetros.");
+console.log ("")
+
+console.log ("ATIVIDADE 5")
+console.log ("")
+var precoTotal = 450; 
+var desconto = precoTotal * 0.1; 
+var precoFinal = precoTotal - desconto; 
+
+console.log("O preço final após um desconto de 10% é: R$" + precoFinal)
